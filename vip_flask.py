9ef2da8a28070402f0e2456e4401b92841352a4d
longfengpili <398745129@qq.com>
2019-09-11 21:53:47 +0800

    port

--- vip_flask.py
+++ vip_flask.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-09-08 13:55:55
-@LastEditTime: 2019-09-11 21:51:29
+@LastEditTime: 2019-09-11 21:53:39
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
@@ -12,5 +12,5 @@
 
 from vip import app
 import webbrowser
-app.run(host='0.0.0.0',port=9537,debug=True)
+app.run(host='0.0.0.0',port=9527,debug=True)
 
