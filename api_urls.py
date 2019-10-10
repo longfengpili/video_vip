589f875b280056e1d94efaa990394ec33f2c9219
longfengpili <398745129@qq.com>
2019-10-11 07:30:28 +0800

    apI

--- api_urls.py
+++ api_urls.py
@@ -9,17 +9,17 @@
 '''
 
 api_urls = {
-    "http://api.yingxiangbao.cn/?url=": "2019-09-21 19:10:28",
-    "http://api.drgxj.com/?url=": "2019-09-10 07:26:29",
-    "http://api.drgxj.com/?url=": "2019-09-10 07:26:29",
-    "http://api.drgxj.com/?url=": "2019-09-10 07:26:29",
-    "http://api.drgxj.com/?url=": "2019-09-10 07:26:29",
+	"http://jx.du2.cc/?url=": "2019-10-11 07:29:11",
+	"http://jx.drgxj.com/?url=": "2019-10-11 07:29:11",
+	"http://jx.618ge.com/?url=": "2019-10-11 07:29:11",
+	"http://vip.jlsprh.com/?url=": "2019-10-11 07:29:11",
+	"http://jx.598110.com/?url=": "2019-10-11 07:29:11",
 }
 
 
 notwork_url = {
-    "http://jx.598110.com/?url=": "2019-09-08 12:55:55",
-    "http://vip.jlsprh.com/?url=": "2019-09-08 12:58:50",
-    "http://jx.618ge.com/?url=": "2019-09-08 13:02:19",
-    "http://jx.drgxj.com/?url=": "2019-09-08 13:02:54",
+    "http://api.yingxiangbao.cn/?url=": "2019-09-21 19:10:28",
 }
+
+
+
