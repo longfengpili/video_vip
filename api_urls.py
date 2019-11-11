ee0ed3507f2c113ed783c6d4d49489de11a79e6a
longfengpili <398745129@qq.com>
2019-11-12 07:54:18 +0800

    [新增]api接口

--- api_urls.py
+++ api_urls.py
@@ -9,16 +9,19 @@
 '''
 
 api_urls = {
-	"http://jx.du2.cc/?url=": "2019-10-11 07:29:11",
-	"http://jx.drgxj.com/?url=": "2019-10-11 07:29:11",
-	"http://jx.618ge.com/?url=": "2019-10-11 07:29:11",
-	"http://vip.jlsprh.com/?url=": "2019-10-11 07:29:11",
-	"http://jx.598110.com/?url=": "2019-10-11 07:29:11",
+	"https://vip.bljiex.com/?v=": "2019-11-12 07:42:47",
+	"https://jiexi.380k.com/?url=": "2019-11-12 07:50:57",
+
 }
 
 
 notwork_url = {
     "http://api.yingxiangbao.cn/?url=": "2019-09-21 19:10:28",
+    "http://jx.du2.cc/?url=": "2019-10-11 07:29:11",
+	"http://jx.drgxj.com/?url=": "2019-10-11 07:29:11",
+	"http://jx.618ge.com/?url=": "2019-10-11 07:29:11",
+	"http://vip.jlsprh.com/?url=": "2019-10-11 07:29:11",
+	"http://jx.598110.com/?url=": "2019-10-11 07:29:11",
 }
 
 
