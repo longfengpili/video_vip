28b3fa60293c0d4cf9115b8692a3e9c31842b74e
longfengpili <398745129@qq.com>
2020-01-02 20:56:19 +0800

    [新增]api接口

--- api_urls.py
+++ api_urls.py
@@ -1,19 +1,37 @@
 '''
 @Author: longfengpili
 @Date: 2019-09-08 12:38:25
-@LastEditTime: 2019-09-21 19:10:38
+@LastEditTime : 2020-01-02 20:55:44
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 @github: https://github.com/longfengpili
 '''
-
+#api：https://github.com/woolition/greasyforks/blob/master/hackVipVideosSet/%E7%A0%B4%E8%A7%A3VIP%E4%BC%9A%E5%91%98%E8%A7%86%E9%A2%91%E9%9B%86%E5%90%88.user.js
 api_urls = {
-	"https://vip.bljiex.com/?v=": "2019-11-12 07:42:47",
-	"https://jiexi.380k.com/?url=": "2019-11-12 07:50:57",
-	"http://jx.du2.cc/?url=": "2019-11-12 08:02:53",
-	"http://jx.618ge.com/?url=": "2019-11-12 08:02:53",
-	"http://vip.jlsprh.com/?url=": "2019-11-12 08:02:53",
+	# "https://vip.bljiex.com/?v=": "2019-11-12 07:42:47",
+	# "https://jiexi.380k.com/?url=": "2019-11-12 07:50:57",
+	# "http://jx.du2.cc/?url=": "2019-11-12 08:02:53",
+	# "http://jx.618ge.com/?url=": "2019-11-12 08:02:53",
+	# "http://vip.jlsprh.com/?url=": "2019-11-12 08:02:53",
+	# "http://jx.618g.com/?url=": "2020-01-02 20:54:38",
+	"http://api.baiyug.cn/vip/index.php?url=": "2020-01-02 20:54:38",
+	"http://tv.wandhi.com/go.html?url=": "2020-01-02 20:54:38",
+	"http://www.zhmdy.top/index.php?zhm_jx=": "2020-01-02 20:54:38",
+	"http://www.pupudy.com/splay.php?play=": "2020-01-02 20:54:38",
+	"http://www.sonimei.cn/?url=": "2020-01-02 20:54:38",
+	"http://aikan-tv.com/?url=": "2020-01-02 20:54:38",
+	"https://jiexi.071811.cc/jx.php?url=": "2020-01-02 20:54:38",
+	"https://660e.com/?url=": "2020-01-02 20:54:38",
+	"http://api.xfsub.com/index.php?url=": "2020-01-02 20:54:38",
+	"https://jiexi.071811.cc/jx.php?url=": "2020-01-02 20:54:38",
+	"http://www.sfsft.com/admin.php?url=": "2020-01-02 20:54:38",
+	"http://q.z.vip.totv.72du.com/?url=": "2020-01-02 20:54:38",
+	"http://jx.api.163ren.com/vod.php?url=": "2020-01-02 20:54:38",
+	"http://www.0335haibo.com/tong.php?url=": "2020-01-02 20:54:38",
+	"http://www.wmxz.wang/video.php?url=": "2020-01-02 20:54:38",
+	"http://www.vipjiexi.com/yun.php?url=": "2020-01-02 20:54:38",
+	"http://v.renrenfabu.com/jiexi.php?url=": "2020-01-02 20:54:38",
 
 }
 
@@ -21,8 +39,6 @@ api_urls = {
 notwork_url = {
     "http://api.yingxiangbao.cn/?url=": "2019-09-21 19:10:28",
 	"http://jx.drgxj.com/?url=": "2019-10-11 07:29:11",
-	
-	
 	"http://jx.598110.com/?url=": "2019-10-11 07:29:11",
 }
 
