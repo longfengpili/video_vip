854651118a22e666020c44de54df482f312667fc
longfengpili <398745129@qq.com>
2020-01-02 21:05:12 +0800

    [新增]api接口

--- api_urls.py
+++ api_urls.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-09-08 12:38:25
-@LastEditTime : 2020-01-02 20:55:44
+@LastEditTime : 2020-01-02 21:04:57
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
@@ -9,29 +9,18 @@
 '''
 #api：https://github.com/woolition/greasyforks/blob/master/hackVipVideosSet/%E7%A0%B4%E8%A7%A3VIP%E4%BC%9A%E5%91%98%E8%A7%86%E9%A2%91%E9%9B%86%E5%90%88.user.js
 api_urls = {
-	# "https://vip.bljiex.com/?v=": "2019-11-12 07:42:47",
-	# "https://jiexi.380k.com/?url=": "2019-11-12 07:50:57",
-	# "http://jx.du2.cc/?url=": "2019-11-12 08:02:53",
-	# "http://jx.618ge.com/?url=": "2019-11-12 08:02:53",
-	# "http://vip.jlsprh.com/?url=": "2019-11-12 08:02:53",
-	# "http://jx.618g.com/?url=": "2020-01-02 20:54:38",
-	"http://api.baiyug.cn/vip/index.php?url=": "2020-01-02 20:54:38",
-	"http://tv.wandhi.com/go.html?url=": "2020-01-02 20:54:38",
-	"http://www.zhmdy.top/index.php?zhm_jx=": "2020-01-02 20:54:38",
-	"http://www.pupudy.com/splay.php?play=": "2020-01-02 20:54:38",
-	"http://www.sonimei.cn/?url=": "2020-01-02 20:54:38",
-	"http://aikan-tv.com/?url=": "2020-01-02 20:54:38",
-	"https://jiexi.071811.cc/jx.php?url=": "2020-01-02 20:54:38",
-	"https://660e.com/?url=": "2020-01-02 20:54:38",
-	"http://api.xfsub.com/index.php?url=": "2020-01-02 20:54:38",
-	"https://jiexi.071811.cc/jx.php?url=": "2020-01-02 20:54:38",
-	"http://www.sfsft.com/admin.php?url=": "2020-01-02 20:54:38",
-	"http://q.z.vip.totv.72du.com/?url=": "2020-01-02 20:54:38",
-	"http://jx.api.163ren.com/vod.php?url=": "2020-01-02 20:54:38",
-	"http://www.0335haibo.com/tong.php?url=": "2020-01-02 20:54:38",
-	"http://www.wmxz.wang/video.php?url=": "2020-01-02 20:54:38",
-	"http://www.vipjiexi.com/yun.php?url=": "2020-01-02 20:54:38",
-	"http://v.renrenfabu.com/jiexi.php?url=": "2020-01-02 20:54:38",
+	"https://vip.bljiex.com/?v=": "2019-11-12 07:42:47",
+	"https://jiexi.380k.com/?url=": "2019-11-12 07:50:57",
+	"http://jx.du2.cc/?url=": "2019-11-12 08:02:53",
+	"http://jx.618ge.com/?url=": "2019-11-12 08:02:53",
+	"http://vip.jlsprh.com/?url=": "2019-11-12 08:02:53",
+	"http://jx.618g.com/?url=": "2020-01-02 20:54:38",
+	"https://www.1717yun.com/jx/ty.php?url=": "2020-01-02 21:04:41",
+	"https://jx.618ge.com/?url=": "2020-01-02 21:04:41",
+	"https://jx.du2.cc/?url=": "2020-01-02 21:04:41",
+	"http://jx.daheiyun.com/?url=": "2020-01-02 21:04:41",
+	"https://vip.jlsprh.com/?url=": "2020-01-02 21:04:41",
+	"https://www.okjx.cc/?url=": "2020-01-02 21:04:41",
 
 }
 
