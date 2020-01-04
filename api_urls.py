2f8ce4efc573fcbf0a83e819b2b76c3043c78e4c
longfengpili <398745129@qq.com>
2020-01-04 21:26:37 +0800

    [调整]api顺序

--- api_urls.py
+++ api_urls.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-09-08 12:38:25
-@LastEditTime : 2020-01-04 12:29:46
+@LastEditTime : 2020-01-04 21:26:02
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
@@ -10,7 +10,6 @@
 #api：https://github.com/woolition/greasyforks/blob/master/hackVipVideosSet/%E7%A0%B4%E8%A7%A3VIP%E4%BC%9A%E5%91%98%E8%A7%86%E9%A2%91%E9%9B%86%E5%90%88.user.js
 api_urls = {
 	"https://www.1717yun.com/jx/ty.php?url=": "2020-01-02 21:04:41",
-	"https://jx.618ge.com/?url=": "2020-01-02 21:04:41",
 	"https://jx.du2.cc/?url=": "2020-01-02 21:04:41",
 	"https://vip.jlsprh.com/?url=": "2020-01-02 21:04:41",
 	"https://www.okjx.cc/?url=": "2020-01-02 21:04:41",
@@ -28,6 +27,7 @@ notwork_url = {
     "http://api.yingxiangbao.cn/?url=": "2019-09-21 19:10:28",
 	"http://jx.drgxj.com/?url=": "2019-10-11 07:29:11",
 	"http://jx.598110.com/?url=": "2019-10-11 07:29:11",
+	"https://jx.618ge.com/?url=": "2020-01-02 21:04:41",
 }
 
 
