dbb83a576d3a048dbe9b2567d247afb3e3dac8b6
longfengpili <398745129@qq.com>
2020-02-23 11:35:59 +0800

    [修改]api

--- api_urls.py
+++ api_urls.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-09-08 12:38:25
-@LastEditTime : 2020-01-12 11:13:23
+@LastEditTime: 2020-02-23 11:35:04
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
@@ -9,17 +9,12 @@
 '''
 #api：https://github.com/woolition/greasyforks/blob/master/hackVipVideosSet/%E7%A0%B4%E8%A7%A3VIP%E4%BC%9A%E5%91%98%E8%A7%86%E9%A2%91%E9%9B%86%E5%90%88.user.js
 api_urls = {
-	"https://jiexi.380k.com/?url=": "2019-11-12 07:50:57",
-	"https://vip.bljiex.com/?v=": "2019-11-12 07:42:47",
-	"http://jx.618g.com/?url=": "2020-01-02 20:54:38",
-	"http://jx.daheiyun.com/?url=": "2020-01-02 21:04:41",
-	"http://jx.du2.cc/?url=": "2019-11-12 08:02:53",
-	"http://jx.618ge.com/?url=": "2019-11-12 08:02:53",
-	"http://vip.jlsprh.com/?url=": "2019-11-12 08:02:53",
-	"https://www.1717yun.com/jx/ty.php?url=": "2020-01-02 21:04:41",
-	"https://jx.du2.cc/?url=": "2020-01-02 21:04:41",
-	"https://vip.jlsprh.com/?url=": "2020-01-02 21:04:41",
-	"https://www.okjx.cc/?url=": "2020-01-02 21:04:41",
+	"http://vip.jlsprh.com/?url=": "2020-02-23 11:34:02",
+	"http://jx.du2.cc/?url=": "2020-02-23 11:34:02",
+	"http://jx.598110.com/?url=": "2020-02-23 11:34:02",
+	"http://jx.618ge.com/?url=": "2020-02-23 11:34:02",
+	"http://jx.drgxj.com/?url=": "2020-02-23 11:34:02",
+	"http://jx.598110.com/?url=": "2020-02-23 11:34:02",
 }
 
 
